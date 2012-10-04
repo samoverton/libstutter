1970166efa0eceff3bf2003315f8278ed5b558f9
Nicolas Favre-Felix <nfelix@acunu.com>
2012-10-04 11:49:02 +0100

    Remove dead code
    --HG--
    extra : convert_revision : e710de077d64014ce0984647a5e657eb67777179

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
-#include "coroutine.h"
 #include "server.h"
-#include <iostream>
-
-using namespace std;
-
-class NumberGenerator : public Coroutine
-{
-	virtual int exec()
-	{
-		yield(0);
-		yield(1);
-		return 42;
-	}
-};
 
 int
 main(int argc, char *argv[])
 {
-	// NumberGenerator g;
-	// for (int i = 0; i < 4; ++i) {
-	// 	cout << "runnable = " << (g.state() != Coroutine::DONE) << endl;
-	// 	cout << "resume: " << g.resume() << endl << endl;
-	// }
-
 	Server s("127.0.0.1", 8888);
 	s.start();
 
